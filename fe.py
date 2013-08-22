4926b1e2ffc285e81bfdac6a3ebe2b460faf2993
Douglas Land <dsl@looprock.com>
2013-08-22 09:51:00 -0500

    Oops, stop calls break stuff.

--- fe.py
+++ fe.py
@@ -48,7 +48,7 @@ def list(root=defaultroot):
 				except:
 					print "Couldn't get child from: %s" % p
 	b = [ rr, d, enabledelete ]
-	zk.stop()
+	#zk.stop()
 	return template('list', res=b)
 
 @route('/edit/<path>')
@@ -56,7 +56,7 @@ def edit(path):
 	p = path.replace("|","/")
 	x = zk.get(p)[0]
 	y = [ p, x]
-	zk.stop()
+	#zk.stop()
 	return template('edit', res=y)
 
 @post('/editsub')
@@ -68,7 +68,7 @@ def editsub():
 	try:
 		zk.set(rp, content)
 		x = [ uf, rp ]	
-		zk.stop()
+		#zk.stop()
 		return template('editsub', res=x)
 		#return "OK: updated!<p><a href=\"/%s\">return to %s</a><p>" % (node,rp)
 	except:
@@ -89,7 +89,7 @@ def createsub():
 			zk.create(fpath, value=content, makepath=True)
 		else:
 			zk.create(fpath, makepath=True)
-		zk.stop()
+		#zk.stop()
 		return template('createsub', res=fpath)
 	except:
 		return "ERROR: unable to create %s!" % fpath
@@ -100,7 +100,7 @@ def delete(path):
 	try:
 		p = path.replace("|","/")
 		zk.delete(p, recursive=True)
-		zk.stop()
+		#zk.stop()
         	return template('delete', res=p)
 	except:
 		return "ERROR: unable to delete %s!" % p
